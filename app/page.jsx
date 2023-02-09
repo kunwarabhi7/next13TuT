15b5943144d2bcc92c1c4812a8f530583ac7e257
Abhishek <abhishekparmar162@gmail.com>
2023-02-09 22:22:34 +0530

    get movie from api

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,15 @@
 
 
-export default function Home() {
+export default async function Home() {
+const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+  const res = await data.json()
+  console.log(res.results)
+
   return (
-    <div className="text-4xl">yo my is abhishek and i am awesome</div>
+    <div className="text-4xl">
+      {res.results.map((movie)=>(
+        <div>{movie.title}</div>
+      ))}
+    </div>
   )
 }
