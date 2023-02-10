050b341dca6d8ec1f3a1f6d290b231f532ab78f8
Abhishek <abhishekparmar162@gmail.com>
2023-02-10 15:40:34 +0530

    added component

--- app/Movie.jsx
+++ app/Movie.jsx
@@ -10,7 +10,7 @@ const Movie = ({title, poster_path , release_date , id }) => {
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-wrap -m-4">
       <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <Link href='#' className="block relative h-48 rounded overflow-hidden">
+        <Link href={`${id}`} className="block relative h-48 rounded overflow-hidden">
           <Image alt="movie" width={800} height={800} className="object-cover object-center w-full h-full block" src={imagePath + poster_path} />
         </Link>
         <div className="mt-4">
--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,9 @@ import Movie from "./Movie"
 export default async function Home() {
 const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
   const res = await data.json()
-  console.log(res.results)
 
   return (
-    <div className="text-4xl">
+    <div className="text-4xl ">
       {res.results.map((movie)=>(
         <Movie key={movie.id} id={movie.id} title={movie.title} poster_path={movie.poster_path} release_date={movie.release_date} />
       ))}
