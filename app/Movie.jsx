88c14b78e9c8d8f05aba5654082b6edc291120a7
Abhishek <abhishekparmar162@gmail.com>
2023-02-11 21:19:03 +0530

    fix css

--- app/Movie.jsx
+++ app/Movie.jsx
@@ -15,7 +15,7 @@ const Movie = ({title, poster_path , release_date , id }) => {
     </Link>
     <div class="p-5">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-800 dark:text-white">{title}</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{release_date}</p>
         <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
